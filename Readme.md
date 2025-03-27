8b3db16595d7319a0f1d0e5ce61d43832bf5500b
liuyuwe5 <liuyuwei012@gmail.com>
2025-03-26 22:24:10 -0400

    add: player left and right move

--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 - 添加主战斗场景 (`Scenes/Game.tscn`)  
 - 添加玩家角色场景 (`Scenes/Player.tscn`)  
 - 使用免费 sprite 作为占位图进行原型开发
+- 映射： A-left， D-right
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 var input
-@export var speed = 100
+@export var speed = 200
 @export var gravity = 10
+@export var animator : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +12,29 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	movement(delta)
+	
+func movement(_delta):
+	input = Input.get_action_strength("right") - Input.get_action_strength("left") 
+	if input != 0:
+		if input>0:
+			velocity.x = speed
+			animator.play("walk")
+		elif input < 0:
+			velocity.x = -speed
+			animator.play("walk")
+	if input == 0:
+		velocity.x = 0
+		animator.play("idle")
+	move_and_slide()
+	#velocity = Input.get_vector("left","right","","") * speed
+	#if velocity == Vector2.ZERO:
+		#animator.play("idle")
+	#else:
+		#animator.play("walk")
+	#move_and_slide()
+
+	#
+	
+	
+	
