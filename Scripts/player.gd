aeec1ad6578f21972c0e80aa5295806c42df59e9
liuyuwe5 <liuyuwei012@gmail.com>
2025-03-26 21:45:17 -0400

    add: player.gd

---
+++ Scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var input
+@export var speed = 100
+@export var gravity = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
