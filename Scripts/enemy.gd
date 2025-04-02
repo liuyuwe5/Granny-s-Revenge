d847faf53305cc3ece3e03d8f6e33e8fdf8033f2
liuyuwe5 <liuyuwei012@gmail.com>
2025-04-01 21:50:57 -0400

    edit: enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -23,7 +23,6 @@ func _on_area_entered(area: Area2D) -> void:
 		is_dead = true
 		area.queue_free()
 		#get_tree().current_scene.score += 1
-
 		
 		await get_tree().create_timer(0.6).timeout
 		queue_free()
